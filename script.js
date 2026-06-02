ffa0c0a374799b6bb0eb26254e6d5ebc45417619
Gunjit Gyawali <gunjitgyawali1@gmail.com>
2026-06-02 10:31:07 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let password = "1111";
+let pass = prompt("Enter Password: ")
+
+while (true) {
+  if (pass != password) {
+    pass = prompt("Wrong Password (Try Again): ")
+  } else {
+    break
+  }
+}
+
 document.getElementById("save-contact").addEventListener("click", function () {
   const vCardData = `
 BEGIN:VCARD
